6b9c7d4376248e784b6be2490b04ea8a32e72935
RainyLiao <liaowenrun@gmail.com>
2022-12-10 18:49:18 +0800

    :rotating_light: Fix some syntax that does not conform to code specifications and styles.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,8 +9,8 @@ import stylelint from 'vite-plugin-stylelint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
-    vueJsx(), 
+    vue(),
+    vueJsx(),
     {
       ...eslint(),
       apply: 'build',
@@ -25,11 +25,11 @@ export default defineConfig({
     },
     stylelint({
       exclude: ['node_modules'],
-    })
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
