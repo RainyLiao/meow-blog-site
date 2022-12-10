c88e8561587adaaa90e0a18fbad4c54448405326
RainyLiao <liaowenrun@gmail.com>
2022-12-10 18:46:28 +0800

    :seedling: Add eslint and stylelint related configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import eslint from 'vite-plugin-eslint'
+import stylelint from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx(), 
+    {
+      ...eslint(),
+      apply: 'build',
+    },
+    {
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: 'serve',
+      enforce: 'post',
+    },
+    stylelint({
+      exclude: ['node_modules'],
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
